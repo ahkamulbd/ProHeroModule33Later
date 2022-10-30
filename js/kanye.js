f568ab2b66bf1622ed10fb669683b9694f982ce5
ahkamulbd <ahkamulbd@gmail.com>
2022-10-30 16:01:36 +0600

    Module 33 initiated to explore API

---
+++ js/kanye.js
@@ -0,0 +1,13 @@
+//console.log('Kanye Mama');
+
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuote(data));
+}
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    console.log(quote);
+    blockQuote.innerText = quote.quote;
+}
