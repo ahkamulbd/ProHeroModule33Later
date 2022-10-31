891c29f696c39a02df0fd28ffc65508c078f5eea
ahkamulbd <ahkamulbd@gmail.com>
2022-10-31 11:34:36 +0600

    Module 33.4 done

--- js/countries.js
+++ js/countries.js
@@ -16,12 +16,29 @@ const displayCountries = countries => {
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
-            <h4>Capital: ${country.capital ? country.capital[0] : 'No Capital'}            
+            <h4>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</h4>  
+            <button onclick = "loadCountryDetail('${country.cca2}')">Details</button>          
         `;
         countriesContainer.appendChild(countryDiv);
     });
 }
+const loadCountryDetail = (code) => {
+    //console.log('get country detail', code);
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    //console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        //.then(data => console.log(data[0]))
+        .then(data => displayCountryDetail(data[0]))
+}
 
-
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+        <h2>Details: ${country.name.common}</h2>
+        <img src = "${country.flags.png}">
+    `;
+}
 
 loadCountries();
